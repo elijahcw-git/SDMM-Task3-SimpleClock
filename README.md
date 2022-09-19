f0c30fbb9e45999fe603b0fb1534b1ca0dae8fc2
elijahcw-git <85442180+elijahcw-git@users.noreply.github.com>
2022-09-18 20:45:56 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SDMM-Task3-SimpleClock
---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+let days = ['Sunday', 'Monday', 'Tuesday', "Wednesday", 'Thursday', 'Friday', 'Saturday']
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let date = new Date();
+
+// console.log(months[date.getMonth()])
+// console.log(days[date.getDay()])
+// console.log(date.getHours())
+
+
